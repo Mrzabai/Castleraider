45c5dee3e19b7a9463003f09023a7fc9a9dae82c
Gelian <gelian92@mail.ru>
2015-12-24 12:31:16 +0100

    Script

---
+++ Script/GameManagerScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManagerScript : MonoBehaviour {
+
+    public static GameManagerScript manager;
+    public int currentMap;
+    public int unlocMap;
+    public int points;
+
+    void Awake ()
+    {
+        manager = this;
+	}
+
+    public void Save()
+    {
+
+    }
+}
---
+++ Script/Movement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public float rotateSpeed = 50.0f;
+
+	void LateUpdate ()
+    {
+        MovementOfCharacter();
+        RotationWithCamera();
+    }
+
+    void MovementOfCharacter()
+    {
+        GetComponent<Rigidbody>().transform.Translate(Vector3.right * Input.GetAxis("Vertical") * speed * Time.deltaTime);
+        GetComponent<Rigidbody>().transform.Translate(Vector3.back * Input.GetAxis("Horizontal") * speed * Time.deltaTime);
+    }
+
+    void RotationWithCamera()
+    {
+        float h = rotateSpeed * Input.GetAxis("Mouse X") * Time.deltaTime;
+        transform.Rotate(0, h, 0);
+    }
+}
